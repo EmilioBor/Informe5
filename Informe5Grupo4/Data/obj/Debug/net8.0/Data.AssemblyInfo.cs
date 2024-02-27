4dd898d2d327ca5c7240f624e9a52b4df442d0be
emilio <emilio3bolivar3@gmail.com>
2024-02-27 14:31:40 -0300

    cambios

--- Informe5Grupo4/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Informe5Grupo4/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad9e67d5f9d77706b8097992a33272968984175d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
